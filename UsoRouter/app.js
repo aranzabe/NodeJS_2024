3e7b18b585f8a7082e165b154846b77e8c800697
Fernando Aranzabe <faranzabe@gmail.com>
2024-01-07 20:15:22 +0100

    7 enero - Router

---
+++ UsoRouter/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const rutaBase = '/api';
+app.use(rutaBase, require('./routes/userRoutes'));
+
+app.listen(process.env.PORT)
+console.log(`Servicio arrancado en ${process.env.PORT}`)
