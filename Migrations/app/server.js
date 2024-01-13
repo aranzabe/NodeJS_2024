181d866056422ad686982af848e5033207b58266
Fernando Aranzabe <faranzabe@gmail.com>
2024-01-13 21:27:54 +0100

    13 ene - Sequelize

---
+++ Migrations/app/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+// const body_parser = require('body-parser');
+const cors = require('cors');
+
+//https://sequelize.org/docs/v6/getting-started/
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.usuariosPath = '/api/usuarios';
+
+        //Middlewares
+        this.middlewares();
+
+        this.routes();
+        
+    }
+
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    routes(){
+        this.app.use(this.usuariosPath , require('../routes/userRoutes'));
+    }
+
+    listen() {
+        this.app.listen(process.env.PORT, () => {
+            console.log(`Servidor escuchando en: ${process.env.PORT}`);
+        })
+    }
+}
+
+module.exports = Server;
